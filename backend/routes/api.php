09a4951507cca86281f65a9518632d49f598a91c
Manish <manishbhakti881@gmail.com>
2026-07-14 00:27:27 +0530

    feat: add comments and real-connections routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FeedController;
 use App\Http\Controllers\InteractionController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RealConnectionsController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +17,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/feed', [FeedController::class, 'index']);
     Route::get('/search', [SearchController::class, 'search']);
     Route::post('/interactions', [InteractionController::class, 'store']);
+    Route::get('/posts/{id}/comments', [CommentController::class, 'index']);
+    Route::post('/posts/{id}/comments', [CommentController::class, 'store']);
+    Route::prefix('real-connections')->group(function () {
+        Route::get('/feed', [RealConnectionsController::class, 'feed']);
+        Route::get('/search', [RealConnectionsController::class, 'search']);
+    });
 });
